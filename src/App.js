e6e0d51032dc827df0637fc8f05463eb2f2bc1cb
Mykenzie Rogers <mykenzierogers@mykenzies-mbp.attlocal.net>
2019-05-27 22:53:38 -0500

    initialize branch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import {Link, Switch} from 'react-router-dom'
-import routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          {console.log(this.props)}
-          {routes}
-          <div>
-            <h1>Nav</h1>
-          </div>
-          <Link to='/profile'><button>Profile</button></Link>
-          <Link to='/'><button>Home</button></Link>
+
       </div>
     );
   }
